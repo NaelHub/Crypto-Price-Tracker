feb4fa23bfecc6987645b5927f917019ac803e7d
Nael Hubaishi <nael.hub@gmail.com>
2022-03-27 09:44:15 -0400

    Homepage & Coin page added

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,12 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import './App.css';
 import Header from './Components/Header';
+import CoinPage from "./Pages/CoinPage";
+import Homepage from "./Pages/Homepage";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        <Header>
+        <Header />
+        <Route path='/' component={Homepage} />
+        <Route path='/coins/:id' component={CoinPage} />
       </div>
     </BrowserRouter>
   );
