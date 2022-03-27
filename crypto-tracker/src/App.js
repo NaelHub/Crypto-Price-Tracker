c41c77c2e50991af1dff28dbd5447438922e5917
Nael Hubaishi <nael.hub@gmail.com>
2022-03-27 11:14:05 -0400

    Style Header

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,30 +1,29 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
 import "./App.css";
-import Header from "./components/Header";
-import Homepage from "./Pages/Homepage";
+import { BrowserRouter, Route } from "react-router-dom";
 import CoinPage from "./Pages/CoinPage";
-import { makeStyles } from "@material-ui/core"
+import Header from "./components/Header";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
-
-  const useStyles = makeStyles(() => ({
-    App: {
-      backgroundColor: "#14161a",
-      color: "white",
-      minHeight: "100vh",
-    }
-  }))
-
-  const classes = useStyles
+  const classes = useStyles();
 
   return (
     <BrowserRouter>
-      <div classname={classes.App}>
+      <div className={classes.App}>
         <Header />
         <Route path="/" component={Homepage} exact />
-        <Route path="/coins:id" component={CoinPage} />
+        <Route path="/coins/:id" component={CoinPage} exact />
       </div>
-    </BrowserRouter >
+    </BrowserRouter>
   );
 }
 
--- crypto-tracker/src/Components/Header.js
+++ crypto-tracker/src/Components/Header.js
@@ -1,7 +1,19 @@
+import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>
+  return
+  (
+    <AppBar color='transparent' position='static'>
+      <Container>
+        <Toolbar>
+          <Typography>
+            Crypto Compass
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
