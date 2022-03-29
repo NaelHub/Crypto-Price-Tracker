2becfdc7f50813eba2726072570cf4658cc05470
barento badaso <badasobarento@gmail.com>
2022-03-29 03:01:34 -0400

    adding changes to header

--- crypto-tracker/src/Components/Header.js
+++ crypto-tracker/src/Components/Header.js
@@ -1,21 +1,40 @@
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}))
+
 const Header = () => {
-  return
-  (
-    <AppBar color='transparent' position='static'>
-      <Container>
-        <Toolbar>
-          <Typography>
-            Crypto Compass
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+
+  const classes = useStyles()
+  return;
+  <AppBar color="transparent" position="static">
+    <Container>
+      <Toolbar>
+        <Typography className={classes.title}>Crypto Compass</Typography>
+        <Select
+          variant="outlined"
+          style={{
+            width: 100,
+            height: 40,
+            marginLeft: 15,
+          }}
+        >
+          <MenuItem>USD</MenuItem>
+          <MenuItem>CAD</MenuItem>
+        </Select>
+      </Toolbar>
+    </Container>
+  </AppBar>;
 };
 
-export default ;
+export default Header;
 
 // testing testing 2
