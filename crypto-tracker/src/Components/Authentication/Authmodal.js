87e861f2b3da5ccb37f99a05afec420990d21316
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2022-03-30 02:24:01 -0400

    setting up alert

--- crypto-tracker/src/Components/Authentication/Authmodal.js
+++ crypto-tracker/src/Components/Authentication/Authmodal.js
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-
+import Login from "./Login"
+import Signup from './Signup';
 const useStyles = makeStyles((theme) => ({
   modal: {
     display: 'flex',
