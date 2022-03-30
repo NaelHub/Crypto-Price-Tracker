703dd21fe7252fe4f5221f31e0ec34c27dcb06b3
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2022-03-30 01:25:27 -0400

    setting up firebase

---
+++ crypto-tracker/src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBrl4jq-xg8JLLB2bM6Ac5EQmolKK7e9MM",
+  authDomain: "crypto-compass-7965e.firebaseapp.com",
+  projectId: "crypto-compass-7965e",
+  storageBucket: "crypto-compass-7965e.appspot.com",
+  messagingSenderId: "512048116134",
+  appId: "1:512048116134:web:d958bff7faac3a18b9df67"
+};
+
+export default firebaseConfig;
