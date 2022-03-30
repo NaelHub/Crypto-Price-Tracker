2bd6853b8f910bb31f5c360dae72bc7ee228e17a
Nael Hubaishi <nael.hub@gmail.com>
2022-03-29 22:29:24 -0400

    Coininfo updates

--- crypto-tracker/src/Components/Banner/CoinInfo.js
+++ crypto-tracker/src/Components/Banner/CoinInfo.js
@@ -1,5 +1,36 @@
-import React from "react";
-const CoinInfo = () => {
-  return <div>coin info</div>;
-};
-export default CoinInfo;
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { CryptoState } from "../CryptoContext";
+import { HistoricalChart } from "../config/api";
+import {
+  CircularProgress,
+
+
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+import { Line } from "react-chartjs-2";
+const CoinInfo ({ coin }) => {
+  const [historicData, setHistoricData] = useState();
+  const [days, setDays] = useState(1);
+  const { currency } = CryptoState();
+
+
+  import axios from "axios";
+  import React, { useEffect, useState } from "react";
+  import { CryptoState } from "../CryptoContext";
+  import { HistoricalChart } from "../config/api";
+  import {
+    CircularProgress,
+
+
+    createTheme,
+    makeStyles,
+    ThemeProvider,
+  } from "@material-ui/core";
+  import { Line } from "react-chartjs-2";
+  const CoinInfo ({ coin }) => {
+    const [historicData, setHistoricData] = useState();
+    const [days, setDays] = useState(1);
+    const { currency } = CryptoState();
