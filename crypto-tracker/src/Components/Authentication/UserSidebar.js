fe840ae29168d0d21870f8be7d5e466e871a7f33
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2022-03-30 03:05:16 -0400

    adding logout to sidebar

--- crypto-tracker/src/Components/Authentication/UserSidebar.js
+++ crypto-tracker/src/Components/Authentication/UserSidebar.js
@@ -2,23 +2,82 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
+import { Avatar, } from "@material-ui/core";
 
 
 const useStyles = makeStyles({
-  list: {
-    width: 250,
+  container: {
+    width: 350,
+    padding: 25,
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    fontFamily: "monospace",
   },
-  fullList: {
-    width: 'auto',
+  profile: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: "20px",
+    height: "92%",
+  },
+  logout: {
+    height: "8%",
+    width: "100%",
+    backgroundColor: "#EEBC1D",
+    marginTop: 20,
+  },
+  picture: {
+    width: 200,
+    height: 200,
+    cursor: "pointer",
+    backgroundColor: "#EEBC1D",
+    objectFit: "contain",
+  },
+  watchlist: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "grey",
+    borderRadius: 10,
+    padding: 15,
+    paddingTop: 10,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 12,
+    overflowY: "scroll",
+  },
+  coin: {
+    padding: 10,
+    borderRadius: 5,
+    color: "black",
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#EEBC1D",
+    boxShadow: "0 0 3px black",
   },
 });
+
+const logOut = () => {
+  signOut(auth);
+  setAlert({
+    open: true,
+    type: "success",
+    message: "Logout Successfull !",
+  });
+
+  toggleDrawer();
+};
 
 export default function UserSidebar() {
   const classes = useStyles();
   const [state, setState] = React.useState({
-    
     right: false,
   });
+  
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -31,14 +90,23 @@ export default function UserSidebar() {
 
   return (
     <div>
-      {['right'].map((anchor) => (
+      {["right"].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
-    </div>
+          <Avatar
+            onClick={toggleDrawer(anchor, true)}
+            style={{
+              height: 38,
+              width: 38,
+              cursor: "pointer",
+              backgroundColor: "#EEBC1D",
+            }}
+            src={user.photoURL}
+            alt={user.displayName || user.email}
+          />
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
   );
 }
