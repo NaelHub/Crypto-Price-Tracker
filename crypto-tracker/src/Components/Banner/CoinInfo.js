2fdf9a11468d0fc7dce16bd1ac3e2b62fd3bbf86
Nael Hubaishi <nael.hub@gmail.com>
2022-03-29 21:20:00 -0400

    CoinPage update & Coininfo added

---
+++ crypto-tracker/src/Components/Banner/CoinInfo.js
@@ -0,0 +1,5 @@
+import React from "react";
+const CoinInfo = () => {
+  return <div>coin info</div>;
+};
+export default CoinInfo;
--- crypto-tracker/src/Pages/CoinPage.js
+++ crypto-tracker/src/Pages/CoinPage.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
-const CoinPage = () => {
-  return (
-    <div>CoinPage</div>
-  )
-}
-
-export default CoinPage
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { SingleCoin } from "../config/api";
+import { CryptoState } from "../CryptoContext"; % D
+const CoinPage () => { % D
+  const { id } = useParams();
+  const [coin, setCoin] useState();
+  const { currency, symbol } CryptoState();
+  async () => {
+                    = await axios.get(SingleCoin(id))
+    const fetchCoin
+    const { data }
+  }
+  return <div>Coin Page</div>;
+};
+export default CoinPage;
