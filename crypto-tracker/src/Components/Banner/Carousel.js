da8180db6e1902628b3376bc4b38c259b4dce2a1
Nael Hubaishi <nael.hub@gmail.com>
2022-03-29 22:34:30 -0400

    carousel & CoinPage update

--- crypto-tracker/src/Components/Banner/Carousel.js
+++ crypto-tracker/src/Components/Banner/Carousel.js
@@ -1,89 +1,101 @@
-import React, { useEffect } from "react";
-import {makeStyles } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
+import { TrendingCoins } from "../../config/api";
 import { CryptoState } from "../../CryptoContext";
-import {TrendingCoins} from "../../config/api"
-import {useState} from 'react';
-import AliceCarousel from 'react-alice-carousel';
-
-const useStyles = makeStyles((theme) => ({
-  Carousel: {
-    height: "50%",
-    display: "flex",
-    alignItems: "center",
-  },
-}));
-
-export function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
+import { numberWithCommas } from "../CoinsTable";
 
 const Carousel = () => {
-
   const [trending, setTrending] = useState([]);
-  const classes = useStyles();
-
-  const { currency, symbol} = CryptoState()
+  const { currency, symbol } = CryptoState();
 
   const fetchTrendingCoins = async () => {
-    const { data } = await axios.get(TrendingCoins(currency))
+    const { data } = await axios.get(TrendingCoins(currency));
+
+    console.log(data);
     setTrending(data);
   };
 
   useEffect(() => {
     fetchTrendingCoins();
-   },[currency]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currency]);
 
-   const items = trending.map((coin) => {
-     let profit = coin.price_change_percentage_24h >= 0;
+  const useStyles = makeStyles((theme) => ({
+    carousel: {
+      height: "50%",
+      display: "flex",
+      alignItems: "center",
+    },
+    carouselItem: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      cursor: "pointer",
+      textTransform: "uppercase",
+      color: "white",
+    },
+  }));
 
-     return (
-       <link className={classes.carouselItem}
-       to={'/coins/${coin.id}'}>
-         <img
-         src= {coin?.image}
-         alt={coin.name}
-         height="80"
-         style={{ marginBottom: 10 }}
-         />
-         <span>
-           {coin?.symbol}
-           &nbsp;
-           <span>
-            {profit && "+"} (coin?.price_change_percentage?.toFixed(2))%
-           </span>
-         </span>
-         <spam style= {{ fontSize: 22, fontWeight: 500 }}>
-           {symbol} {numberWithComas(coin?.current_price.toFixed(2))}
-           </spam>
-       </link>
-     );
-   })
+  const classes = useStyles();
 
-   const responsive = {
-     0: {
-       items: 2,
-     },
-     512: {
-       items: 4,
-     },
-   };
+  const items = trending.map((coin) => {
+    let profit = coin?.price_change_percentage_24h >= 0;
 
-  return 
-    <div className = {classes.carousel}>
-     <AliceCarousel
-      mouseTracking
-      infinite 
-      autoPlayInterval= {1000}
-      animationDuration= {1500}
-      disableDotsControls
-      disableButtonsControls
-      responsive={responsive}
-      autoPlay
-      items={items}
->
-    </div>;
-  
+    return (
+      <Link className={classes.carouselItem} to={`/coins/${coin.id}`}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+        <span>
+          {coin?.symbol}
+          &nbsp;
+          <span
+            style={{
+              color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+              fontWeight: 500,
+            }}
+          >
+            {profit && "+"}
+            {coin?.price_change_percentage_24h?.toFixed(2)}%
+          </span>
+        </span>
+        <span style={{ fontSize: 22, fontWeight: 500 }}>
+          {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
+      </Link>
+    );
+  });
+
+  const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    },
+  };
+
+  return (
+    <div className={classes.carousel}>
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        disableButtonsControls
+        responsive={responsive}
+        items={items}
+        autoPlay
+      />
+    </div>
+  );
 };
 
-export default Carousel
+export default Carousel;
