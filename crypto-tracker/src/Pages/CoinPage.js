149310231cfe919e182e6793f03d8ab55ba7d6f6
Ibrahim Mohamed <ia.mohamed02@gmail.com>
2022-03-30 03:44:09 -0400

    fixed functionalities to watchlist

--- crypto-tracker/src/Pages/CoinPage.js
+++ crypto-tracker/src/Pages/CoinPage.js
@@ -8,22 +8,71 @@ import { SingleCoin } from "../config/api";
 import { numberWithCommas } from "../components/CoinsTable";
 import { CryptoState } from "../CryptoContext";
 import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const CoinPage = () => {
   const { id } = useParams();
   const [coin, setCoin] = useState();
 
-  const { currency, symbol, user } = CryptoState();
- 
+  const { currency, symbol, user, setAlert, watchlist } = CryptoState();
+
   const fetchCoin = async () => {
     const { data } = await axios.get(SingleCoin(id));
 
     setCoin(data);
   };
 
+  const inWatchlist = watchlist.includes(coin?.id);
+
+  const addToWatchlist = async () => {
+    const coinRef = doc(db, "watchlist", user.uid);
+    try {
+      await setDoc(
+        coinRef,
+        { coins: watchlist ? [...watchlist, coin?.id] : [coin?.id] },
+        { merge: true }
+      );
+
+      setAlert({
+        open: true,
+        message: `${coin.name} Added to the Watchlist !`,
+        type: "success",
+      });
+    } catch (error) {
+      setAlert({
+        open: true,
+        message: error.message,
+        type: "error",
+      });
+    }
+  };
+
+  const removeFromWatchlist = async () => {
+    const coinRef = doc(db, "watchlist", user.uid);
+    try {
+      await setDoc(
+        coinRef,
+        { coins: watchlist.filter((wish) => wish !== coin?.id) },
+        { merge: true }
+      );
+
+      setAlert({
+        open: true,
+        message: `${coin.name} Removed from the Watchlist !`,
+        type: "success",
+      });
+    } catch (error) {
+      setAlert({
+        open: true,
+        message: error.message,
+        type: "error",
+      });
+    }
+  };
+
   useEffect(() => {
     fetchCoin();
-   
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const useStyles = makeStyles((theme) => ({
@@ -65,9 +114,6 @@ const CoinPage = () => {
       width: "100%",
       [theme.breakpoints.down("md")]: {
         display: "flex",
-        justifyContent: "space-around",
-      },
-      [theme.breakpoints.down("sm")]: {
         flexDirection: "column",
         alignItems: "center",
       },
@@ -76,52 +122,6 @@ const CoinPage = () => {
       },
     },
   }));
-
-  const addToWatchlist = async () => {
-    const coinRef = doc(db, "watchlist", user.uid);
-    try {
-      await setDoc(
-        coinRef,
-        { coins: watchlist ? [...watchlist, coin?.id] : [coin?.id] },
-        { merge: true }
-      );
-      setAlert({
-        open: true,
-        message: `${coin.name} Added to the Watchlist !`,
-        type: "success",
-      });
-    } catch (error) {
-      setAlert({
-        open: true,
-        message: error.message,
-        type: "error",
-      });
-    }
-  };
-
-  const removeFromWatchlist = async () => {
-    const coinRef = doc(db, "watchlist", user.uid);
-    try {
-      await setDoc(
-        coinRef,
-        { coins: watchlist.filter((wish) => wish !== coin?.id) },
-        { merge: true }
-      );
-
-      setAlert({
-        open: true,
-        message: `${coin.name} Removed from the Watchlist !`,
-        type: "success",
-      });
-    } catch (error) {
-      setAlert({
-        open: true,
-        message: error.message,
-        type: "error",
-      });
-    }
-  };
-
 
   const classes = useStyles();
 
@@ -157,7 +157,6 @@ const CoinPage = () => {
               {numberWithCommas(coin?.market_cap_rank)}
             </Typography>
           </span>
-
           <span style={{ display: "flex" }}>
             <Typography variant="h5" className={classes.heading}>
               Current Price:
@@ -195,7 +194,6 @@ const CoinPage = () => {
               M
             </Typography>
           </span>
-
           {user && (
             <Button
               variant="outlined"
